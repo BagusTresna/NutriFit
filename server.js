e923c54a61abdf128379f380242ba428cef553cd
BagusTresna <kadekbagustresna@gmail.com>
2024-11-24 21:44:00 +0800

    Updating server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const Hapi = require('@hapi/hapi');
-const userRoutes = require('./user-api/routes'); // Impor rute dari routes.js
+const userRoutes = require('./user-api/routes');
 const articleRoutes = require('./article-api/routes');
 
 const init = async() => {
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
+        port: process.env.PORT || 8000, // Gunakan PORT dari lingkungan
+        host: '0.0.0.0', // Host harus 0.0.0.0 untuk App Engine
     });
 
+    // Tambahkan rute
     server.route([...userRoutes, ...articleRoutes]);
 
+    // Mulai server
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
 };
